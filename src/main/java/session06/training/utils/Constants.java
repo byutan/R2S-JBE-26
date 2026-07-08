5d81556d494f1ac697b3ae0b4c7af7ebf3072d7f
byutan <nguyenhthang1234@gmail.com>
2026-07-09 00:30:02 +0900

    update(topic-3/oop): redone session_06 deliveries

--- src/main/java/session06/training/utils/Constants.java
+++ src/main/java/session06/training/utils/Constants.java
@@ -1,25 +1,19 @@
 package session06.training.utils;
 
 public final class Constants {
-    public Constants() {};
+    // Code
+    public static final String REGEX = "^RA\\d{3}$";
 
-    public static final String REGEX_CODE = "^RA[0-9][0-9][0-9]$";
-    public static final byte ACTIVE_STATUS = 1;
-    public static final byte INACTIVE_STATUS = 0;
+    // Status
+    public static final String ACTIVE_STATUS = "active";
+    public static final String INACTIVE_STATUS = "inactive";
 
-    public static final String OPTIONAL = "optional";
-    public static final String PREREQUISITE = "prerequisite";
-    public static final String NONE = "N/A";
-    public static final byte MIN_DURATION = 0;
+    // Flag
+    public static final String OPTIONAL_FLAG = "optional";
+    public static final String PREREQUISITE_FLAG = "prerequisite";
+    public static final String NONE_FLAG = "N/A";
 
-    public static final String TYPE_CODE = "code";
-    public static final String TYPE_NAME = "name";
-    public static final String TYPE_STATUS = "status";
-    public static final String TYPE_DURATION = "duration";
-    public static final String TYPE_FLAG = "flag";
+    // Duration
+    public static final short MIN = 1;
 
-    public static final int CREATE_COURSE = 1;
-    public static final int SEARCH_COURSE = 2;
-    public static final int DISPLAY_COURSE = 3;
-    public static final int QUIT = 4;
 }
